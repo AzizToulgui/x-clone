2ff57a9fb35dd8cf60fae6255bf643fb18b06159
Aziz <aziztoulgui20@gmail.com>
2025-02-26 20:57:00 +0100

    begin sensitive logic

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -27,7 +27,7 @@ const Share = () => {
 
 
     return (
-        <form className="p-4 flex gap-4" action={shareAction}>
+        <form className="p-4 flex gap-4" action={(formData)=>shareAction(formData, settings)}>
             {/*Avatar*/}
             <div className="relative w-10 h-10 rounded-full overflow-hidden">
                 <Image path={"general/avatar.png"} alt={""} w={100} h={100} tr={true}/>
@@ -40,7 +40,13 @@ const Share = () => {
                 {/*Preview Image*/}
                 {
                     previewURL && <div className="relative rounded-xl overflow-hidden">
-                        <NextImage src={previewURL} alt="" width={600} height={600}/>
+                        <NextImage src={previewURL} alt="" width={600} height={600} className={`w-full ${
+                            settings.type === "original"
+                                ? "h-full object-contain"
+                                : settings.type === "square"
+                                    ? "aspect-square object-cover"
+                                    : "aspect-video object-cover"
+                        }`}/>
                         <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white py-1 px-4 rounded-full font-bold test-sm cursor-pointer" onClick={()=> setIsEditorOpen(true)}>Edit</div>
                     </div>
                 }
