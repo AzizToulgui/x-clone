8830fb3214aed0595b6a115325de63a82050fbf1
Aziz <aziztoulgui20@gmail.com>
2025-02-27 22:17:49 +0100

    begin working on rightbar

---
+++ src/components/PopularTags.tsx
@@ -0,0 +1,14 @@
+
+const PopularTags = () => {
+    return (
+        <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4 ">
+            <h1 className="text-xl font-bold text-textGrayLight ">What&#39;s Happening</h1>
+            {/*Trends*/}
+            <div className=""></div>
+            {/*Topics*/}
+            <div className=""></div>
+        </div>
+    )
+}
+export default PopularTags
+                                        /*2:18:02*/
