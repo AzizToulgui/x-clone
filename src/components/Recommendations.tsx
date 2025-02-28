c5d8603b24dc6f371672403ac93b6057013807c2
Aziz <aziztoulgui20@gmail.com>
2025-02-28 11:33:02 +0100

    finish working on rightbar and start working on profile page

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,7 +1,56 @@
+import Link from "next/link";
+import Image from "@/components/Image";
 
 const Recommendations = () => {
     return (
-        <div>Recommendations</div>
+        <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+            {/*User Card*/}
+            <div className="flex items-center justify-between ">
+                {/*Image and User info*/}
+                <div className="flex items-center gap-2 ">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10 ">
+                        <Image path="general/avatar.png" alt="John Doe" w={100} h={100} tr={true}/>
+                    </div>
+                    <div className="">
+                        <h1 className="text-md font-bold">John Doe</h1>
+                        <span className="text-textGray text-sm ">@John_Doe </span>
+                    </div>
+                </div>
+                {/*Button*/}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">Follow</button>
+            </div>
+            {/*User Card*/}
+            <div className="flex items-center justify-between ">
+                {/*Image and User info*/}
+                <div className="flex items-center gap-2 ">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10 ">
+                        <Image path="general/avatar.png" alt="John Doe" w={100} h={100} tr={true}/>
+                    </div>
+                    <div className="">
+                        <h1 className="text-md font-bold">John Doe</h1>
+                        <span className="text-textGray text-sm ">@John_Doe </span>
+                    </div>
+                </div>
+                {/*Button*/}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">Follow</button>
+            </div>
+            {/*User Card*/}
+            <div className="flex items-center justify-between ">
+                {/*Image and User info*/}
+                <div className="flex items-center gap-2 ">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10 ">
+                        <Image path="general/avatar.png" alt="John Doe" w={100} h={100} tr={true}/>
+                    </div>
+                    <div className="">
+                        <h1 className="text-md font-bold">John Doe</h1>
+                        <span className="text-textGray text-sm ">@John_Doe </span>
+                    </div>
+                </div>
+                {/*Button*/}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">Follow</button>
+            </div>
+            <Link href="/" className="text-iconBlue ">Show More</Link>
+        </div>
     )
 }
 export default Recommendations
