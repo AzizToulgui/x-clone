c5ca5d6dd2e19d5014088b8d488e9d97258f0ec7
Aziz <aziztoulgui20@gmail.com>
2025-03-01 18:02:28 +0100

    parallel routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,37 @@
-import "./globals.css";
 import LeftBar from "@/components/LeftBar";
+import "./globals.css";
 import RightBar from "@/components/RightBar";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'L',
+    description: 'Next.js social media application project',
+}
+
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                       modal
+                                   }: Readonly<{
+    children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-sreen-xl xxl:max-w-screen-xxl mx-auto flex justify-between ">
+    return (
+        <html lang="en">
+        <body>
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
             <div className="px-2 xsm:px-4 xxl:px-8 ">
                 <LeftBar />
             </div>
-            <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
+            <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
                 {children}
+                {modal}
             </div>
-            <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
+            <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
                 <RightBar />
             </div>
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
